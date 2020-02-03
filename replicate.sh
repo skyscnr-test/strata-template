3bf49721673fe069771fe2c2a592383f481ee911
Michael Roddy <michael.roddy@skyscanner.net>
2020-02-03 17:31:48 +0000

    Update replicate.sh

--- replicate.sh
+++ replicate.sh
@@ -5,5 +5,5 @@ ls -ltha
 
 git config --global user.name "$CODECOMMIT_USER"
 git config --global user.password "$CODECOMMIT_PW"
-git remote add codecommit https://$CODECOMMIT_USER:$CODECOMMIT_PW@git-codecommit.eu-west-2.amazonaws.com/v1/repos/public-dns-strata
+git remote add codecommit https://$CODECOMMIT_USER:$CODECOMMIT_PW@git-codecommit.eu-west-2.amazonaws.com/v1/repos/template-strata
 git push codecommit master
