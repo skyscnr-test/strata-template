46a1fdb9d838f3227f7811777d4a2aed4b27f41a
Stewart Wallace <stewart.wallace@skyscanner.net>
2020-09-23 11:08:34 +0100

    Update stratum.py

--- stratum.py
+++ stratum.py
@@ -5,32 +5,24 @@ from StrataCore.strata_cdk import StrataApp
 
 from template.stacks.template_stack import TemplateStack
 
+def account_level(context: StrataApp, account_id):
+    template_stack = TemplateStack(context, 'template-stack')
+    return [template_stack]
 
-def get_app(config: Optional[StratumConfig]):
 
+def region_level(context: StrataApp, stratum: StratumConfig, region: str):
+    template_stack = TemplateStack(context, 'template-stack', stratum, env={'region': stratum.region})
+    return [template_stack]
+
+
+def az_level(context: StrataApp, stratum: StratumConfig, az: str):
+    template_stack = TemplateStack(context, 'template-stack', stratum, env={'region': stratum.region})
+    return [template_stack]
+
+
+def run(config: Optional[StratumConfig]):
     app = StrataApp(config=config)
-
-
-    def account_level(context: StrataApp, account_id):
-
-        template_stack = TemplateStack(context, 'template-stack')
-        return [template_stack]
-
-    def region_level(context: StrataApp, stratum: StratumConfig, region: str):
-        template_stack = TemplateStack(context, 'template-stack', stratum, env={'region': stratum.region})
-        return [template_stack]
-
-
-    def az_level(context: StrataApp, stratum: StratumConfig, az: str):
-
-        template_stack = TemplateStack(context, 'template-stack', stratum, env={'region': stratum.region})
-        return [template_stack]
-
-
     app.per_account(account_level)
-
     app.per_region(region_level)
-
     app.per_az(az_level)
-
     return app
